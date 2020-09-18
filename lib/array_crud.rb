f6e3672032a6e6bb0a09431b4f4a7d0cf825d505
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-18 18:46:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.unshift ("Rebecca")
+  add_element_to_end_of_array.unshift ("Rebecca")
 end
 
 def add_element_to_start_of_array(array, element)
