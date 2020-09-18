1fac7e21c71a4ebf87c973462d0e5700a5f06df6
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-18 19:08:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   names = ["Gustavo", "Cameron", "Ashley", "Mike"]
-  names.pop "arrays!"
+  names.pop 
 end
 
 def remove_element_from_start_of_array(array)
