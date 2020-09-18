23a3a5851735aa40cfff66b5761db4ed22eeb2fa
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-18 18:50:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["Gustavo", "Cameron", "Ashley", "Mike"]
-  element.unshift ("Rebecca")
+  array=["Gustavo", "Cameron", "Ashley", "Mike"]
+
 end
 
 def add_element_to_start_of_array(array, element)
