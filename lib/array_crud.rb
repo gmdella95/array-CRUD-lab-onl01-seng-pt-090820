96fcdfd98131e9031dbc4f9212944c6c3b238972
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-18 18:51:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array=["Gustavo", "Cameron", "Ashley", "Mike"]
-
+  element = "Rebecca"
 end
 
 def add_element_to_start_of_array(array, element)
